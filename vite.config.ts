0348edb124a854f7e7fc8382e85c5103f3cafe48
svon <svon.me@gmail.com>
2023-09-02 10:58:15 +0800

    feat: iconfont

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
+import jsx from "@vitejs/plugin-vue-jsx";
+import htmlPlugin from "vite-plugin-html-config";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  cacheDir: ".cache",
-  resolve: {
-    extensions: [".ts", ".vue", ".js", ".tsx"],
-    alias: {
-      "src/": `${path.resolve(__dirname, "src")}/`,
+export default defineConfig(function({ mode }) {
+  const env = loadEnv(mode, "./");
+  
+  const headScripts = [];
+  // 引入 iconfont
+  if (env.VITE_APP_ICONFONT) {
+    headScripts.push({ defer: true, src: env.VITE_APP_ICONFONT });
+  }
+  
+  return {
+    cacheDir: ".cache",
+    resolve: {
+      extensions: [".ts", ".vue", ".js", ".tsx"],
+      alias: {
+        "src/": `${path.resolve(__dirname, "src")}/`,
+      },
     },
-  },
-  plugins: [
-    vue()
-  ]
+    plugins: [
+      vue(), 
+      jsx(),
+      htmlPlugin({ headScripts }),
+    ]
+  }
 })
